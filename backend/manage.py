0121c92eb0f121f5f7d65c93c3e66afec721274f
Sara Kathleen <engsarasilvan@gmail.com>
2026-06-08 20:43:26 -0300

    Update manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-"""Utilitário de linha de comando do Django para tarefas administrativas."""
 import os
 import sys
 
@@ -10,8 +8,7 @@ def main():
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Não foi possível importar o Django. Verifique se ele está instalado "
-            "e se o ambiente virtual está ativado."
+
         ) from exc
     execute_from_command_line(sys.argv)
 
